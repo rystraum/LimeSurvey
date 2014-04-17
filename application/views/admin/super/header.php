e734655b6cf1e679310018f2a6a666de6ba5bb0d
Rystraum Gamonez <rystraum@gmail.com>
2014-04-17 22:32:20 +0800

    Added font-awesome in local styles

--- application/views/admin/super/header.php
+++ application/views/admin/super/header.php
@@ -21,12 +21,12 @@
             App()->getClientScript()->registerCssFile(Yii::app()->getConfig('adminstyleurl') . "adminstyle-rtl.css" );
         }
         App()->getClientScript()->registerCssFile(Yii::app()->getConfig('adminstyleurl') . "printablestyle.css", 'print');
+        App()->getClientScript()->registerCssFile(Yii::app()->getConfig('publicstyleurl') . 'font-awesome/css/font-awesome.min.css');
     ?>
     <?php echo $datepickerlang;?>
     <title><?php echo $sitename;?></title>
     <link rel="shortcut icon" href="<?php echo $baseurl;?>styles/favicon.ico" type="image/x-icon" />
     <link rel="icon" href="<?php echo $baseurl;?>styles/favicon.ico" type="image/x-icon" />
-    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
     <?php echo $firebug ?>
     <?php $this->widget('ext.LimeScript.LimeScript'); ?>
     <?php $this->widget('ext.LimeDebug.LimeDebug'); ?>
