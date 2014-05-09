f895c45957c5376639c204186435be8b42106b3e
Rystraum Gamonez <rystraum@gmail.com>
2014-05-09 12:08:41 +0800

    Changed migration.sql

--- migration.sql
+++ migration.sql
@@ -1,1 +1,2 @@
-alter table lime_surveys add column custom_url varchar(255);
+UPDATE lime_settings_global SET stg_value = "FormsGen" WHERE stg_name = "sitename";
+ALTER TABLE lime_surveys ADD COLUMN custom_url varchar(255);
